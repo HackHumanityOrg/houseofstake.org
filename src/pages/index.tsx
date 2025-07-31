c5cf36dd48e9d9d3c928c868e315d9f2330ba1d4
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-07-31 17:14:36 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            This is a fork test
           </Link>
         </div>
       </div>
